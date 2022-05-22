f8334dd03ee2cbdd548ffcfd6e13c4fcfc53358c
bruh-moment00 <87185750+bruh-moment00@users.noreply.github.com>
2022-05-22 14:01:00 +0300

    Added models for client side

---
+++ Orders Payments Client/Orders_Payments_Client.HttpClient/Models/Funds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders_Payments_Client.HttpClient.Models
+{
+    public class Funds
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Sum { get; set; }
+        public double Remain { get; set; }
+    }
+}
