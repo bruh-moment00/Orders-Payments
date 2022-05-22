e8bcf2a7cc15cc86c84165a65b84e8f45d7a4792
bruh-moment00 <87185750+bruh-moment00@users.noreply.github.com>
2022-05-22 16:32:44 +0300

    Created MVP Pattern

---
+++ Orders Payments Client/Orders_Payments_Client.HttpClient/Funds/Repositories/Interfaces/IFundsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Orders_Payments_Client.API.Funds.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders_Payments_Client.API.Funds.Repositories.Interfaces
+{
+    public interface IFundsRepository
+    {
+        public IEnumerable<Fund> GetFunds();
+        public string PostFunds(IEnumerable<Fund> newFunds);
+    }
+}
